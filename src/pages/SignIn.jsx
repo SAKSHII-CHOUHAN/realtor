1577832adce3b8d7d57d5fafff997656d455f16e
Sakshi Chouhan <sakshichouhan016@gmail.com>
2024-02-26 20:09:24 +0530

    create private route and custom hook for protecting profile page and logout funtionality

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -37,7 +37,7 @@ export default function SignIn() {
     <section>
       <h1 className='text-3xl text-center mt-6 font-bold'>Sign In</h1>
       <div className='flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto'>
-        <div className='md:w-[67%] lg:w[50%] mb-12 md:mb-6'>
+        <div className='md:w-[67%] lg:w-[50%] mb-12 md:mb-6'>
           <img src='https://images.unsplash.com/photo-1560518883-ce09059eeffa?q=80&w=1973&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           alt='key'
           className='w-full rounded-2xl'/>
